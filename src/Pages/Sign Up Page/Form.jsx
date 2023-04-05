09a92cb968635ccd110a3900383cd74844d89025
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-04-05 13:27:59 +0530

    All functionalities completed

--- src/Pages/Sign Up Page/Form.jsx
+++ src/Pages/Sign Up Page/Form.jsx
@@ -1,15 +1,9 @@
 import React, {useState, useRef, useEffect} from 'react'
 import './Form.css'
 import Error from './Error'
-import Warnings from './Warnings';
+
 
 export default function () {
-    const Name = useRef();
-    const Username = useRef();
-    const Email = useRef();
-    const Phonenumber = useRef();
-    const Terms = useRef();
-
     const [valid, setValid] = useState(false)
 
     const [form, setForm] = useState({
@@ -28,16 +22,23 @@ export default function () {
         terms: ""
     })
 
-    const [warnText, setWarnText] = useState({
-        name: "",
-        username: "",
-        email: "",
-        phonenumber: "",
-        terms: ""
-    })
+    const handleChangeTerms = ()=>{
+        setForm(prev => ({...prev, terms: true}))
+        if(form.terms==true)
+        setForm(prev => ({...prev, terms: false}))
+        setErrText(prev => ({...prev, terms: ""}))
+    }
 
     let handleChange = (event) => {
-        setForm({...form, [event.target.name] : event.target.value})  
+        setForm({...form, [event.target.name] : event.target.value})
+        if(event.target.name=="name")
+        setErrText(prev=>({...prev, name:""}))
+        if(event.target.name=="username")
+        setErrText(prev=>({...prev, username:""}))
+        if(event.target.name=="email")
+        setErrText(prev=>({...prev, email:""}))
+        if(event.target.name=="phonenumber")
+        setErrText(prev=>({...prev, phonenumber:""}))
     }
 
     const validate = (e) => {
@@ -47,7 +48,7 @@ export default function () {
 
     function check() {
         let k=0;
-        if(!form.name || !form.username || !form.email || !form.phonenumber) { 
+        if(!form.name || !form.username || !form.email || !form.phonenumber || !form.terms) { 
         console.log("not found")
         if(!form.name)
         setErrText({...errText, name:"Please enter a name"})
@@ -57,35 +58,43 @@ export default function () {
         setErrText((prev)=>({...prev , email: "Please enter an email"}))
         if(!form.phonenumber)
         setErrText((prev)=>({...prev , phonenumber: "Please enter a phone number"}))
+        if(!form.terms)
+        setErrText((prev)=>({...prev , terms: "Please agree to the above terms."}))
         setValid(false)
         }
         else if(form.email.search("@")==-1) {
-        setErrText({...errText , email: "Please enter a correct email."})
-    }
+        setErrText({...errText , email: "Please enter a correct email with @ sign."})
+        }
+        else if(/\d/.test(form.phonenumber)==false) {
+        setErrText((prev)=>({...prev , phonenumber: "Please enter a valid phone number."}))
+        }
+        else if(form.phonenumber.length<=3) {
+            setErrText((prev)=>({...prev , phonenumber: "Phone number should be of more than 3 digits."}))
+        }
         else setValid(true)
     }
 
   return (
     <div>
         <form action="/" onSubmit={validate}>
-            <input type="text" ref={Name} name="name" id="name" value={form.name} placeholder='Name' onChange={handleChange} minLength={2} required/>
+            <input type="text" name="name" id="name" value={form.name} placeholder='Name' onChange={handleChange} minLength={2} required/>
             <div>
                 <Error message={errText.name}/>
             </div>
-            <input type="text" ref={Username} name="username" id="username" value={form.username} placeholder='Username' maxLength={40} onChange={handleChange} required/>
+            <input type="text" name="username" id="username" value={form.username} placeholder='Username' maxLength={40} onChange={handleChange} required/>
             <div>
                 <Error message={errText.username}/>
             </div>
-            <input type="email" ref={Email} name='email' id='email' placeholder='Email' value={form.email} onChange={handleChange} required/>
+            <input type="email" name='email' id='email' placeholder='Email' value={form.email} onChange={handleChange} required/>
             <div>
                 <Error message={errText.email} />
             </div>
-            <input type="tel" ref={Phonenumber} name='phonenumber' id='phonenumber' placeholder='Mobile' pattern='[0-9]{10}' value={form.phonenumber} onChange={handleChange} required />
+            <input type="tel" name='phonenumber' id='phonenumber' placeholder='Mobile' pattern='[0-9]{10}' value={form.phonenumber} onChange={handleChange} required />
             <div>
                 <Error message={errText.phonenumber} />
             </div>
             <div>
-            <input type="checkbox" ref={Terms} name='sharedata' id='sharedata' value={form.terms} onChange={handleChange} required/>
+            <input type="checkbox" name='termsValue' id='termsValue' value={form.terms} checked={form.terms} onChange={handleChangeTerms} required/>
             <label htmlFor="sharedata">Share my registeration data with superapp</label>
             <div><Error message={errText.terms} /></div>
             </div>
--- src/Pages/Sign Up Page/Warnings.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Warnings(props) {
-  return (
-    <span style={{color: "yellow", fontSize: "12px"}}>{props.message}</span>
-  )
-}
