00ddfcd55b9a9f5a5783e2c5248117f4d7bd3017
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-04-12 16:08:40 +0530

    React Routing Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import SignUp from './Pages/Sign Up Page/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <SignUp />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/category-page" element={<h1>HELLO</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
